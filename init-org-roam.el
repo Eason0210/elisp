6ef149b2850067500b0ea35115584f582efff3bf
Eason0210 <aqua0210@163.com>
2021-07-18 20:00:06 +0800

    update init-org-roam.el

--- init-org-roam.el
+++ init-org-roam.el
@@ -5,7 +5,7 @@
 (when (and (executable-find "sqlite3")
            (executable-find "cc"))
   (when (maybe-require-package 'org-roam)
-    (setq org-roam-directory (file-truename "~/org/org-roam"))
+    (setq org-roam-directory (file-truename "~/.org/org-roam"))
     (setq org-roam-v2-ack t)
     (setq org-roam-db-gc-threshold most-positive-fixnum)
     
@@ -13,14 +13,12 @@
     (global-set-key (kbd "C-c n f") 'org-roam-node-find)
     (global-set-key (kbd "C-c n i") 'org-roam-node-insert)
     (global-set-key (kbd "C-c n r") 'org-roam-ref-find)
-
-    (with-eval-after-load 'org-roam
-      (org-roam-setup)
-      
-      (global-set-key (kbd "C-c n l") 'org-roam-buffer-toggle)
-      (global-set-key (kbd "C-c n g") 'org-roam-graph)
-      (global-set-key (kbd "C-c n j") 'org-roam-dailies-capture-today))
-    
+    (global-set-key (kbd "C-c n l") 'org-roam-buffer-toggle)
+    (global-set-key (kbd "C-c n g") 'org-roam-graph)
+    (global-set-key (kbd "C-c n j") 'org-roam-dailies-capture-today))
+  
+  (with-eval-after-load 'org-roam
+    (org-roam-setup)
     (with-eval-after-load 'org-protocol
       (require 'org-roam-protocol))))
 
